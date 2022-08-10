d8000cb78b395b1e94cd79f7576f0f39c0fd0472
FreeDoM <jstrfaheem065@gmail.com>
2022-08-11 01:13:06 +0530

    Single update query statement f*k,

--- rclient/src/screens/CreateOrEditUser.js
+++ rclient/src/screens/CreateOrEditUser.js
@@ -34,21 +34,33 @@ const CreateOrEditUser = ({ user, setUser }) => {
             createUserMutation({
               variables: { createUserDto: { name, age: parseInt(age) } },
               update: (cache, { data }) => {
-                const cachedUsers = cache.readQuery({
-                  // variables: { ageDivider: 40 },
-                  query: GET_ALL_USERS_QUERY,
-                });
-                cache.writeQuery({
-                  query: GET_ALL_USERS_QUERY,
-                  // variables: { ageDivider: 40 },
-                  data: {
-                    getAllUsers: [
-                      ...cachedUsers?.getAllUsers,
-                      data?.createUser,
-                    ],
-                  },
-                  // broadcast: false
-                });
+                // const cachedUsers = cache.readQuery({
+                //   // variables: { ageDivider: 40 },
+                //   query: GET_ALL_USERS_QUERY,
+                // });
+                // cache.writeQuery({
+                //   query: GET_ALL_USERS_QUERY,
+                //   // variables: { ageDivider: 40 },
+                //   data: {
+                //     getAllUsers: [
+                //       ...cachedUsers?.getAllUsers,
+                //       data?.createUser,
+                //     ],
+                //   },
+                //   // broadcast: false
+                // });
+                cache.updateQuery(
+                  { query: GET_ALL_USERS_QUERY },
+                  (cachedUsers) => {
+                    console.log({ updateQueryCache: data });
+                    return {
+                      getAllUsers: [
+                        ...cachedUsers?.getAllUsers,
+                        data?.createUser,
+                      ],
+                    };
+                  }
+                );
               },
             });
           }
